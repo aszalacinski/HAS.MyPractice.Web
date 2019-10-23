3ef78149ae7ce8e09c4e5cc0eb1a274ede3dca6e
Aarron Szalacinski <aarron.szalacinski@happyappsoftware.com>
2019-10-22 20:06:09 -0400

    Gets Alpha Nav working!

---
+++ Pages/Admin/ManageProfiles.cshtml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace HAS.MyPractice.Web.Pages.Admin
+{
+    public class ManageProfilesModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
