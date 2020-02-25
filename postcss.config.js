8f8cc0b83a361385cb07cf5dba57c903ca5dbc7b
Aarron Szalacinski <aarron.szalacinski@happyappsoftware.com>
2020-02-25 15:18:08 -0500

    adds production settings to webpack

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,21 @@
-﻿module.exports = {
+﻿const purgecss = require('@fullhuman/postcss-purgecss')({
+
+    // Specify the paths to all of the template files in your project 
+    content: [
+        './src/**/*.html',
+        './**/*.html',
+        './**/*.cshtml'
+    ],
+
+    // Include any special characters you're using in this regular expression
+    defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || []
+})
+
+module.exports = {
     plugins: [
         require('postcss-import'),
         require('tailwindcss'),
         require('autoprefixer'),
+        ...(process.env.NODE_ENV === 'production' ? [purgecss] : [])
     ]
 }
--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,20 @@
 ﻿const path = require('path');
+const TerserJSPlugin = require('terser-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 
 module.exports = (env, arg) => {
     return {
-        mode: "development",
+        mode: arg.mode === "production" ? "production" : "development",
         entry: ['./Scripts/css/styles.css', './Scripts/ts/App.ts'],
         output: {
             filename: 'site.js',
             path: path.resolve(__dirname, './wwwroot/js')
         },
-        devtool: 'inline-source-map',
+        devtool: arg.mode === "production" ? 'none' : 'inline-source-map',
+        optimization: {
+            minimizer: arg.mode === "production" ? [new TerserJSPlugin({}), new OptimizeCSSAssetsPlugin({})] : []
+        },
         module: {
             rules: [
                 {
